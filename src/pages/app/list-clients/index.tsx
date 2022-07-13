383d44205472791cc4159071d8454cd1bb73d3bc
gustavocaldeira40 <86030388+gustavocaldeira40@users.noreply.github.com>
2022-07-12 22:00:43 -0300

    changes

--- src/pages/app/list-clients/index.tsx
+++ src/pages/app/list-clients/index.tsx
@@ -3,91 +3,98 @@ import HeaderDefault from 'components/HeaderDefault'
 import Menu from 'components/Menu'
 import React, { useState } from 'react'
 import {
-  Container,
-  ContainerCenter,
-  ContainerContent,
-  ContainerMenu,
-  ContainerTextMenu,
-  TextMenu,
+    Container,
+    ContainerCenter,
+    ContainerContent,
+    ContainerMenu,
+    ContainerTextMenu,
+    TextMenu,
 } from './style'
 
 type TypeOptions = {
-  selected: 'inProgress' | 'approved'
+    selected: 'inProgress' | 'approved'
 }
 
 const ListClients: React.FC = () => {
-  /*
-   *   CONTEXT
-   */
+    /*
+     *   CONTEXT
+     */
 
-  /*
-   *   REFS
-   */
+    /*
+     *   REFS
+     */
 
-  /*
-   *   STATES
-   */
+    /*
+     *   STATES
+     */
 
-  const [showInformation, setShowInformation] = useState(false)
-  const [options, setOptions] = useState<TypeOptions>({
-    selected: 'inProgress',
-  })
+    const [showInformation, setShowInformation] = useState(false)
+    const [options, setOptions] = useState<TypeOptions>({
+        selected: 'inProgress',
+    })
 
-  /*
-   *   HOOKS
-   */
+    /*
+     *   HOOKS
+     */
 
-  /*
-   *   LAYOUT
-   */
+    /*
+     *   LAYOUT
+     */
 
-  /*
-   *   FORMIK
-   */
+    /*
+     *   FORMIK
+     */
 
-  /*
-   *   FUNCTIONS
-   */
+    /*
+     *   FUNCTIONS
+     */
 
-  /*
-   *   EFFECTS
-   */
+    /*
+     *   EFFECTS
+     */
 
-  return (
-    <Container xl={12}>
-      <Header
-        withRightSide
-        logoOnLeft
-        titleOneWord="List "
-        titleSecondWord="Clients"
-      />
-      <ContainerCenter>
-        <HeaderDefault
-          titleMain="Welcome the List of clients"
-          showInformation={showInformation}
-          setShowInformation={setShowInformation}
-        />
-        <ContainerMenu>
-          <ContainerTextMenu>
-            <TextMenu
-              onClick={() => setOptions({ ...options, selected: 'inProgress' })}
-              isFocused={options.selected === 'inProgress'}
-            >
-              In Progress
-            </TextMenu>
-            <TextMenu
-              onClick={() => setOptions({ ...options, selected: 'approved' })}
-              isFocused={options.selected === 'approved'}
-              marginRight="50%"
-            >
-              Approved
-            </TextMenu>
-          </ContainerTextMenu>
-          <Menu />
-        </ContainerMenu>
-      </ContainerCenter>
-    </Container>
-  )
+    return (
+        <Container xl={12}>
+            <Header
+                withRightSide
+                logoOnLeft
+                titleOneWord="List "
+                titleSecondWord="Clients"
+            />
+            <ContainerCenter>
+                <HeaderDefault
+                    titleMain="Welcome the List of clients"
+                    showInformation={showInformation}
+                    setShowInformation={setShowInformation}
+                />
+                <ContainerMenu>
+                    <ContainerTextMenu>
+                        <TextMenu
+                            onClick={() =>
+                                setOptions({
+                                    ...options,
+                                    selected: 'inProgress',
+                                })
+                            }
+                            isFocused={options.selected === 'inProgress'}
+                        >
+                            In Progress
+                        </TextMenu>
+                        <TextMenu
+                            onClick={() =>
+                                setOptions({ ...options, selected: 'approved' })
+                            }
+                            isFocused={options.selected === 'approved'}
+                            marginRight="50%"
+                        >
+                            Approved
+                        </TextMenu>
+                    </ContainerTextMenu>
+                    <Menu />
+                </ContainerMenu>
+            </ContainerCenter>
+        </Container>
+    )
 }
 
 export default ListClients
--- src/pages/app/register-clients/index.tsx
+++ src/pages/app/register-clients/index.tsx
@@ -15,235 +15,256 @@ import LinkComponent from 'components/Link'
 import ButtonComponent from 'components/Button'
 import { Colors } from 'styles/colors'
 import {
-  Container,
-  ContainerAddress,
-  ContainerButtonRightSide,
-  ContainerButtons,
-  ContainerCenter,
-  ContainerContent,
-  ContainerDocument,
-  ContainerIcon,
-  ContainerMessageField,
-  ContainerRow,
-  Icon,
-  IconAddress,
-  Separator,
-  TextDescription,
-  TextIcon,
+    Container,
+    ContainerAddress,
+    ContainerButtonRightSide,
+    ContainerButtons,
+    ContainerCenter,
+    ContainerContent,
+    ContainerDocument,
+    ContainerIcon,
+    ContainerMessageField,
+    ContainerRow,
+    Icon,
+    IconAddress,
+    Separator,
+    TextDescription,
+    TextIcon,
 } from './style'
 
 const RegisterClients: React.FC = () => {
-  /*
-   *   CONTEXT
-   */
+    /*
+     *   CONTEXT
+     */
 
-  /*
-   *   REFS
-   */
-  const corporateNameRef = useRef(null)
-  const documentRef = useRef(null)
-  const addressRef = useRef(null)
-  const numberRef = useRef(null)
-  const cityRef = useRef(null)
-  const stateRef = useRef(null)
+    /*
+     *   REFS
+     */
+    const corporateNameRef = useRef(null)
+    const documentRef = useRef(null)
+    const addressRef = useRef(null)
+    const numberRef = useRef(null)
+    const cityRef = useRef(null)
+    const stateRef = useRef(null)
 
-  /*
-   *   STATES
-   */
+    /*
+     *   STATES
+     */
 
-  const [showInformation, setShowInformation] = useState(false)
-  const [check, setCheck] = useState(false)
-  const [disabled, setDisabled] = useState(true)
+    const [showInformation, setShowInformation] = useState(false)
+    const [check, setCheck] = useState(false)
+    const [disabled, setDisabled] = useState(true)
 
-  /*
-   *   HOOKS
-   */
+    /*
+     *   HOOKS
+     */
 
-  /*
-   *   LAYOUT
-   */
+    /*
+     *   LAYOUT
+     */
 
-  /*
-   *   FORMIK
-   */
-  const {
-    errors,
-    handleSubmit,
-    resetForm,
-    isValid,
-    values,
-    handleChange,
-    handleBlur,
-  } = useFormik({
-    initialValues: {
-      corporate_name: '',
-      document: '',
-      address: '',
-      number: '',
-      city: '',
-      state: '',
-    },
-    enableReinitialize: true,
-    validationSchema: yup.object().shape({
-      corportate_name: yup.string().required('Inform the your corportate name'),
-      document: yup.string().required('Inform the your document'),
-      address: yup.string().required('Inform the your address'),
-    }),
-    onSubmit: async (_values, { setSubmitting }) => {
-      setSubmitting(true)
-    },
-  })
+    /*
+     *   FORMIK
+     */
+    const {
+        errors,
+        handleSubmit,
+        resetForm,
+        isValid,
+        values,
+        handleChange,
+        handleBlur,
+    } = useFormik({
+        initialValues: {
+            corporate_name: '',
+            document: '',
+            address: '',
+            number: '',
+            city: '',
+            state: '',
+        },
+        enableReinitialize: true,
+        validationSchema: yup.object().shape({
+            corportate_name: yup
+                .string()
+                .required('Inform the your corportate name'),
+            document: yup.string().required('Inform the your document'),
+            address: yup.string().required('Inform the your address'),
+        }),
+        onSubmit: async (_values, { setSubmitting }) => {
+            setSubmitting(true)
+        },
+    })
 
-  /*
-   *   FUNCTIONS
-   */
+    /*
+     *   FUNCTIONS
+     */
 
-  /*
-   *   EFFECTS
-   */
+    /*
+     *   EFFECTS
+     */
 
-  return (
-    <Container>
-      <Header
-        withRightSide
-        logoOnLeft
-        titleOneWord="New "
-        titleSecondWord="Client"
-      />
+    return (
+        <Container>
+            <Header
+                withRightSide
+                logoOnLeft
+                titleOneWord="New "
+                titleSecondWord="Client"
+            />
 
-      <ContainerCenter>
-        <BlackContainer>
-          <HeaderDefault
-            fontSize="1.5em"
-            titleMain="Let’s go create a new client ? "
-            showInformation={showInformation}
-            setShowInformation={setShowInformation}
-            withMenu
-          />
-          <ContainerContent>
-            <TextDescription>Fill in the fields below</TextDescription>
-            <ContainerRow xl={12}>
-              <InputComponent
-                passRef={corporateNameRef}
-                defaultValue={values.corporate_name}
-                onChange={handleChange('corporate_name')}
-                placeholder="Comporate Name"
-                errorMessage={errors.corporate_name}
-              />
-              <Separator />
-              <ContainerDocument>
-                <InputMaskComponent
-                  passRef={documentRef}
-                  width="65%"
-                  defaultValue={values.document}
-                  value={values.document}
-                  onChange={handleChange('document')}
-                  placeholder="Document"
-                  errorMessage={errors.document}
-                />
+            <ContainerCenter>
+                <BlackContainer>
+                    <HeaderDefault
+                        fontSize="1.5em"
+                        titleMain="Let’s go create a new client ? "
+                        showInformation={showInformation}
+                        setShowInformation={setShowInformation}
+                        withMenu
+                    />
+                    <ContainerContent>
+                        <TextDescription>
+                            Fill in the fields below
+                        </TextDescription>
+                        <ContainerRow xl={12}>
+                            <InputComponent
+                                passRef={corporateNameRef}
+                                defaultValue={values.corporate_name}
+                                onChange={handleChange('corporate_name')}
+                                placeholder="Comporate Name"
+                                errorMessage={errors.corporate_name}
+                            />
+                            <Separator />
+                            <ContainerDocument>
+                                <InputMaskComponent
+                                    passRef={documentRef}
+                                    width="65%"
+                                    defaultValue={values.document}
+                                    value={values.document}
+                                    onChange={handleChange('document')}
+                                    placeholder="Document"
+                                    errorMessage={errors.document}
+                                />
 
-                {check ? (
-                  <ContainerIcon>
-                    <TextIcon isCheck={check}>Avaliable User</TextIcon>
-                    <Icon src={CheckIcon} alt="check icon" />
-                  </ContainerIcon>
-                ) : (
-                  <ContainerIcon>
-                    <TextIcon isCheck={check}>User not avaliable</TextIcon>
-                    <Icon src={ErrorIcon} alt="error icon" />
-                  </ContainerIcon>
-                )}
-              </ContainerDocument>
-              <ContainerMessageField>
-                <ContainerAddress>
-                  <InputComponent
-                    passRef={addressRef}
-                    defaultValue={values.address}
-                    onChange={handleChange('address')}
-                    placeholder="Address"
-                    errorMessage={errors.address}
-                    width="35vw"
-                    withoutMB
-                  />
-                  <IconAddress src={SearchRounded} alt="search rounded" />
-                </ContainerAddress>
-                <TextDescription withMarginBottom withoutMT isSmall>
-                  Enter your address to autocomplete
-                </TextDescription>
-              </ContainerMessageField>
-              <Separator />
-              <InputComponent
-                passRef={numberRef}
-                defaultValue={values.number}
-                onChange={handleChange('number')}
-                placeholder="N.º"
-                errorMessage={errors.number}
-                isInputNumber
-                maxLength={5}
-                width="5vw"
-                disabled={disabled}
-                style={{
-                  backgroundColor: disabled
-                    ? `${Colors.bgButton}50`
-                    : Colors.bgButton,
-                }}
-              />
-              <Separator />
-              <InputComponent
-                passRef={cityRef}
-                defaultValue={values.city}
-                onChange={handleChange('city')}
-                placeholder="City"
-                errorMessage={errors.city}
-                width="15vw"
-                disabled={disabled}
-                style={{
-                  backgroundColor: disabled
-                    ? `${Colors.bgButton}50`
-                    : Colors.bgButton,
-                }}
-              />
-              <Separator />
-              <InputComponent
-                passRef={stateRef}
-                defaultValue={values.state}
-                onChange={handleChange('state')}
-                placeholder="State"
-                errorMessage={errors.state}
-                width="10vw"
-                disabled={disabled}
-                style={{
-                  backgroundColor: disabled
-                    ? `${Colors.bgButton}50`
-                    : Colors.bgButton,
-                }}
-              />
-            </ContainerRow>
-          </ContainerContent>
-          <ContainerButtons>
-            <LinkComponent path="/clients">
-              <ButtonComponent>Back</ButtonComponent>
-            </LinkComponent>
-            <ContainerButtonRightSide>
-              <ButtonComponent
-                onClick={() => resetForm()}
-                margin="0 20px"
-                outlined
-              >
-                Clear Fields
-              </ButtonComponent>
+                                {check ? (
+                                    <ContainerIcon>
+                                        <TextIcon isCheck={check}>
+                                            Avaliable User
+                                        </TextIcon>
+                                        <Icon
+                                            src={CheckIcon}
+                                            alt="check icon"
+                                        />
+                                    </ContainerIcon>
+                                ) : (
+                                    <ContainerIcon>
+                                        <TextIcon isCheck={check}>
+                                            User not avaliable
+                                        </TextIcon>
+                                        <Icon
+                                            src={ErrorIcon}
+                                            alt="error icon"
+                                        />
+                                    </ContainerIcon>
+                                )}
+                            </ContainerDocument>
+                            <ContainerMessageField>
+                                <ContainerAddress>
+                                    <InputComponent
+                                        passRef={addressRef}
+                                        defaultValue={values.address}
+                                        onChange={handleChange('address')}
+                                        placeholder="Address"
+                                        errorMessage={errors.address}
+                                        width="35vw"
+                                        withoutMB
+                                    />
+                                    <IconAddress
+                                        src={SearchRounded}
+                                        alt="search rounded"
+                                    />
+                                </ContainerAddress>
+                                <TextDescription
+                                    withMarginBottom
+                                    withoutMT
+                                    isSmall
+                                >
+                                    Enter your address to autocomplete
+                                </TextDescription>
+                            </ContainerMessageField>
+                            <Separator />
+                            <InputComponent
+                                passRef={numberRef}
+                                defaultValue={values.number}
+                                onChange={handleChange('number')}
+                                placeholder="N.º"
+                                errorMessage={errors.number}
+                                isInputNumber
+                                maxLength={5}
+                                width="5vw"
+                                disabled={disabled}
+                                style={{
+                                    backgroundColor: disabled
+                                        ? `${Colors.bgButton}50`
+                                        : Colors.bgButton,
+                                }}
+                            />
+                            <Separator />
+                            <InputComponent
+                                passRef={cityRef}
+                                defaultValue={values.city}
+                                onChange={handleChange('city')}
+                                placeholder="City"
+                                errorMessage={errors.city}
+                                width="15vw"
+                                disabled={disabled}
+                                style={{
+                                    backgroundColor: disabled
+                                        ? `${Colors.bgButton}50`
+                                        : Colors.bgButton,
+                                }}
+                            />
+                            <Separator />
+                            <InputComponent
+                                passRef={stateRef}
+                                defaultValue={values.state}
+                                onChange={handleChange('state')}
+                                placeholder="State"
+                                errorMessage={errors.state}
+                                width="10vw"
+                                disabled={disabled}
+                                style={{
+                                    backgroundColor: disabled
+                                        ? `${Colors.bgButton}50`
+                                        : Colors.bgButton,
+                                }}
+                            />
+                        </ContainerRow>
+                    </ContainerContent>
+                    <ContainerButtons>
+                        <LinkComponent path="/clients">
+                            <ButtonComponent>Back</ButtonComponent>
+                        </LinkComponent>
+                        <ContainerButtonRightSide>
+                            <ButtonComponent
+                                onClick={() => resetForm()}
+                                margin="0 20px"
+                                outlined
+                            >
+                                Clear Fields
+                            </ButtonComponent>
 
-              <LinkComponent path="/clients">
-                <ButtonComponent disabled={!isValid} secondary>
-                  Save
-                </ButtonComponent>
-              </LinkComponent>
-            </ContainerButtonRightSide>
-          </ContainerButtons>
-        </BlackContainer>
-      </ContainerCenter>
-    </Container>
-  )
+                            <LinkComponent path="/clients">
+                                <ButtonComponent disabled={!isValid} secondary>
+                                    Save
+                                </ButtonComponent>
+                            </LinkComponent>
+                        </ContainerButtonRightSide>
+                    </ContainerButtons>
+                </BlackContainer>
+            </ContainerCenter>
+        </Container>
+    )
 }
 
 export default RegisterClients
