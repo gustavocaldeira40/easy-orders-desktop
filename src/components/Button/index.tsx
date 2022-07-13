e3e28c7ab0c197b5a6d119bc4d735a1e6f8b4ec2
gustavocaldeira40 <86030388+gustavocaldeira40@users.noreply.github.com>
2022-07-12 21:15:26 -0300

    changes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 import { ButtonPersonalized } from './style'
 
 const ButtonComponent: React.FC<ButtonProps> = ({
-  children,
-  outlined,
-  onClick,
-  secondary,
-  margin,
+    children,
+    outlined,
+    onClick,
+    secondary,
+    margin,
 }) => {
-  return (
-    <ButtonPersonalized
-      secondary={secondary}
-      outlined={outlined}
-      onClick={onClick}
-      margin={margin}
-    >
-      {children}
-    </ButtonPersonalized>
-  )
+    return (
+        <ButtonPersonalized
+            secondary={secondary}
+            outlined={outlined}
+            onClick={onClick}
+            margin={margin}
+        >
+            {children}
+        </ButtonPersonalized>
+    )
 }
 
 export default ButtonComponent
--- src/pages/app/sales/index.tsx
+++ src/pages/app/sales/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SalesOrders: React.FC = () => {
-  return <div>SalesOrders</div>
+    return <div>SalesOrders</div>
 }
 
 export default SalesOrders
